c7d3ffea8ba0a2e199ba0a22ded3bea6ab2883a6
Pravali-2-7 <pravalikapotta000@gmail.com>
2025-11-14 23:16:19 +0530

    Added login function

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-function signup(userName) {
-    let users = ["pavani", "sravanthi", "likhitha", "pravalika"];
-
-    if (users.includes(userName)) {
-        return "User Already Registered, Please Login";
-    } else {
-        users.push(userName);
-        return "Signup Successful, Please Login";
-    }
-}
+def signup():
+    username = input("Create username: ")
+    password = input("Create password: ")
+    return username, password
 
 
-console.log(signup("Pravlika"));    
-console.log(signup("Ramya"));      
+def login(saved_username, saved_password):
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    if username == saved_username and password == saved_password:
+        print("Login successful!")
+    else:
+        print("Login failed! Incorrect username or password.")
+
+
+# --- Main Program ---
+u, p = signup()
+login(u, p)
